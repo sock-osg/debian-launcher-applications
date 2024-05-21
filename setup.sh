41f540bbe51ca07a44938b96557ed029126e6c9c
Samuel Quintana <sock.sqt@gmail.com>
2024-05-21 17:32:37 -0600

    feat: Clean up applications

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SETUP_DIR=`pwd`
+
+cd /usr/share/applications
+
+#sudo ln -s $SETUP_DIR/postman.desktop postman.desktop
+#sudo ln -s $SETUP_DIR/arduino.desktop arduino.desktop
+#sudo ln -s $SETUP_DIR/telegram.desktop telegram.desktop
+
+cd -
